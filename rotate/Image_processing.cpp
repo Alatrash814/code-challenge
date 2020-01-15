793068354f3f26d6d85b62d99605ba276cd5afcd
unknown <Alatrash814@gmail.com>
2020-01-16 01:23:41 +0200

    Q 3 Done!

--- rotate/Image_processing.cpp
+++ rotate/Image_processing.cpp
@@ -5,24 +5,48 @@ using namespace std;
 
 int main (int argc, char * argv[]){
 	
-	FILE * inputFile = fopen("intput.txt", "r");
-	char line[1024];
+	FILE * inputFile = fopen("input.txt", "r");
+	FILE * outputFile = fopen("output.txt", "w");
+	if (!inputFile)
+		cout << "No such a file" << endl;
 	
-	int maxLength = 0;
-	int numberOfRows = 0;
-	
-	while (fgets(line, sizeof(line), inputFile))
-	{
-		numberOfRows++;
-		if (strlen(line) > maxLength)
-			maxLength = strlen(line);	
-	}
+	else {
 		
-	char ** input = new char *[numberOfRows];
-	
-	for (int i = 0; i < numberOfRows; i++)
-	{
-		input[i] = new char [maxLength];
+		char line[1024];
 		
+		int maxLength = 0;
+		int numberOfRows = 0;
+		
+		while (fgets(line, sizeof(line), inputFile))
+		{
+			numberOfRows++;
+			if (strlen(line) > maxLength)
+				maxLength = strlen(line);	
+		}
+		
+		char ** input = new char *[numberOfRows];
+		
+		rewind (inputFile);
+		
+		for (int i = 0; i < numberOfRows; i++)
+		{
+			
+			input[i] = new char [maxLength];
+			fgets(line, sizeof(line), inputFile);
+			strcpy(input[i], line);
+			
+			for (int j = strlen(input[i]) - 1; j < maxLength; j++)
+				input[i][j] = ' ';
+				
+			input[i][maxLength] = NULL;
+				
+		}
+		
+		for (int i = 0 ; i < maxLength; i++)
+		{
+			for (int j = 0 ; j < numberOfRows; j++)
+				fprintf(outputFile,"%c",input[j][i]);
+			fprintf(outputFile,"%\n");
+		}
 	}
 }
